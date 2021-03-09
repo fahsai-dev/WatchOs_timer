96bc8a4d1a12f86069f1b47ca7b2f2035927bc96
Fahsai <fahsai@antinode.co.th>
2021-03-09 11:39:03 +0700

    first commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  watchOS_timer WatchKit Extension
+//
+//  Created by cupcode on 9/3/2564 BE.
+//  Copyright © 2564 Cupcode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watchOS_timer WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  codeTutorials_watchOS_timerBasics WatchKit Extension
+//
+//  Created by Christopher Guirguis on 4/19/20.
+//  Copyright © 2020 Christopher Guirguis. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
