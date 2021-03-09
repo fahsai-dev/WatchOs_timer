c0ef0cdacf23103bf69f51755d8052836aa70a7e
Fahsai <fahsai@antinode.co.th>
2021-03-09 09:50:13 +0700

    Initial Commit

---
+++ watchOS_timer WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  watchOS_timer WatchKit Extension
+//
+//  Created by cupcode on 9/3/2564 BE.
+//  Copyright © 2564 Cupcode. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
